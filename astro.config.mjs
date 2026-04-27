9246678e39f769f991d904bae5a9608406992e43
RomanEger <egerromaan@gmail.com>
2026-04-27 22:16:09 +0600

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import { siteConfig } from './src/config/site.js';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  
+
+  site: siteConfig.baseUrl,
+  
+  // Output static site
+  output: 'static',
+  
+  // Integrations
+  integrations: [
+    sitemap(),
+  ],
+});
